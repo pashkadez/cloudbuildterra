99e3496f2841f06548238fce062ad975fb932f45
pashkadez <pashkadez@gmail.com>
2022-01-19 19:42:35 +0200

    WIP

--- jenkins.tf
+++ jenkins.tf
@@ -9,9 +9,12 @@ resource "helm_release" "jenkins" {
   values = [templatefile("jenkins-values/values.yaml",{
     project = var.project
     bucket = var.bucket
-    db_pass = "${data.google_secret_manager_secret_version.db_pass.secret_data}"
     zone = var.zone
     clust_name = google_container_cluster.terraform-builder-gcs-backend.name
+    db_host = google_sql_database_instance.issuedb.first_ip_address
+    db_user = google_sql_user.user.name
+    db_pass = data.google_secret_manager_secret_version.db_pass.secret_data
+    slack_wh = data.google_secret_manager_secret_version.slack_wh.secret_data
   })]
 } 
 
